9278224d90decde4613dbb02c932c0d9775c5679
Yekaterina Grabar <cagrab@Catherines-MacBook-Pro-2.local>
2018-05-19 13:27:06 -0700

    Added command line imagesnap

--- face-id/index.js
+++ face-id/index.js
@@ -1,10 +1,15 @@
 const path = require('path')
 const fs = require('fs')
 const fr = require('face-recognition')
+const find = require('find-process');
 
 const dataPath = path.resolve('./data/faces')
 const classNames = ['sheldon', 'lennard', 'raj', 'howard', 'stuart']
 
+const exec = require('child_process').exec;
+
+
+/*
 const allFiles = fs.readdirSync(dataPath)
 const imagesByClass = classNames.map(c =>
   allFiles
@@ -16,7 +21,7 @@ const imagesByClass = classNames.map(c =>
 const numTrainingFaces = 10
 const trainDataByClass = imagesByClass.map(imgs => imgs.slice(0, numTrainingFaces))
 const testDataByClass = imagesByClass.map(imgs => imgs.slice(numTrainingFaces))
-
+*/
 
 const sound = require('./sound');
 
@@ -26,11 +31,24 @@ sound.playSound(name);
 
 function mainLoop() {
   // Takes image with webcam
+  startImagesnap();
   // Uses image to guess identity
   // Logic to confirm identity (e.g. 2 identifications sequentially)
   // Respond to identity with playing sound
   // Respond to identity with light
 
+}
+
+function startImagesnap() {
+  find('name', 'imagesnap')
+    .then(function (list) {
+      console.log('there are %s imagesnap process(es)', list.length);
+      if(list.length === 0) {
+        console.log("Now executing the following shell command: ");
+        console.log("imagesnap -t 5");
+        exec('imagesnap -t 5');        
+      }
+  });
 }
 
 setInterval(mainLoop, 3000);
